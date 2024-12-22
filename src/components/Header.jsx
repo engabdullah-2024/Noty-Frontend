f268e289bf654408040ebcf2341e7ff644f96ab8
engabdullah-2024 <enga95311@gmail.com>
2024-12-22 20:26:28 +0300

    Updated Header.jsx, added for Menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { useState } from 'react';
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
     return (
-        <header className='bg-blue-500 text-white px-20 py-6 flex justify-between w-full items-center'>
+        <header className='bg-blue-500 text-white px-6 py-4 flex justify-between items-center w-full'>
             <h1 className='text-3xl font-bold'>Welcome to Noty</h1>
-           <Link to="/add-note"><h1>Add Note  +</h1></Link> 
-           <Link to="/notes"><h1> Notes</h1></Link> 
-           <Link to="/register"><h1> Register Now</h1></Link> 
-           <Link to="/login"><h1> Login Now</h1></Link> 
+            <div className='md:hidden' onClick={toggleMenu}>
+                {menuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+            </div>
+            <nav className={`md:flex ${menuOpen ? 'block' : 'hidden'} w-full md:w-auto`}>
+                <Link to="/add-note" className='block md:inline-block px-4 py-2'>Add Note +</Link>
+                <Link to="/notes" className='block md:inline-block px-4 py-2'>Notes</Link>
+                <Link to="/register" className='block md:inline-block px-4 py-2'>Register Now</Link>
+                <Link to="/login" className='block md:inline-block px-4 py-2'>Login Now</Link>
+                <Link to="/sidenav" className='block md:inline-block px-4 py-2'>SideNav</Link>
+            </nav>
         </header>
     );
 };
